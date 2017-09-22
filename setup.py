90fc11f4b1f0386dc353548fdf149e3bbe46486d
JohnGorm <jack.m.gman@gmail.com>
2017-09-22 12:35:35 -0500

    Files to package app

--- setup.py
+++ setup.py
@@ -5,13 +5,13 @@ setup(name='sciunit_webpage_oauth',
       description='Sciunit-CLI website with Oauth callback route',
       keywords='sciunit flask geotrust',
       url='https://bitbucket.org/geotrust/sciunit-website-2',
+      author='John Gorman',
       author_email='jack.m.gman@gmail.com',
       license='MIT',
       packages=['sciunit_webpage_oauth'],
       install_requires=[
           'flask',
           'flask_mail',
-          'os',
           'gevent'
       ],
       zip_safe=False)
