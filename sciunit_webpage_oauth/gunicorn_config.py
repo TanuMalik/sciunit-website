7dbc6935efc7533a275ffd3cae5739cee29aa53f
JohnGorm <jack.m.gman@gmail.com>
2017-10-02 16:09:01 -0500

    Change pid file location to /var/run subdirectory

--- sciunit_webpage_oauth/gunicorn_config.py
+++ sciunit_webpage_oauth/gunicorn_config.py
@@ -3,4 +3,4 @@ import os
 bind = ['127.0.0.1:5000']
 worker_class = 'gevent'
 workers = 2
-pidfile = os.path.join(os.path.dirname(os.getcwd()), "oauth.pid")
+pidfile = "/var/run/sciunit_website/sciunit_website.pid"
