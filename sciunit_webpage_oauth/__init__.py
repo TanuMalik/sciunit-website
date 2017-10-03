fde00d34352cdc188d232b0fd6b07d5dcc078913
Zhihao Yuan <lichray@gmail.com>
2017-10-03 17:57:12 +0000

    PEP8 clean

--- sciunit_webpage_oauth/__init__.py
+++ sciunit_webpage_oauth/__init__.py
@@ -1,13 +1,12 @@
-from flask import Flask, url_for, request, make_response, render_template, abort
+from flask import Flask, url_for, request, render_template, abort
 from flask_mail import Mail, Message
 import os
-
 
 
 app = Flask(__name__)
 mail = Mail(app)
 
-app.config['MAIL_SERVER']='smtp.mailtrap.io'
+app.config['MAIL_SERVER'] = 'smtp.mailtrap.io'
 app.config['MAIL_PORT'] = 2525
 app.config['MAIL_USERNAME'] = '93eb3942ae0e1f'
 app.config['MAIL_PASSWORD'] = '65e58901acaab7'
@@ -16,19 +15,23 @@ mail = Mail(app)
 
 @app.route("/")
 def homepage():
-	return app.send_static_file('index.html')
+    return app.send_static_file('index.html')
+
 
 @app.route('/download/')
 def downloadpage():
-	return app.send_static_file('download.html')
+    return app.send_static_file('download.html')
+
 
 @app.route('/docs/')
 def docspage():
-	return app.send_static_file('docs.html')
+    return app.send_static_file('docs.html')
+
 
 @app.route('/papers/')
 def paperspage():
-	return app.send_static_file('papers.html')
+    return app.send_static_file('papers.html')
+
 
 @app.route('/support/', methods=['GET', 'POST'])
 def supportpage():
@@ -36,24 +39,19 @@ def supportpage():
         name = request.form['name']
         user_email = request.form['email']
         subject = request.form['subject']
-        message = 'A message from ' + name + '\nMessage: ' + request.form['message']
-        email_msg = Message(subject = subject, sender=user_email,
-        	recipients =['jack.m.gman@gmail.com'],
-        	body = message,
-        	reply_to = user_email)
+        message = 'A message from %s\nMessage: %s' % (
+            name, request.form['message'])
+        email_msg = Message(
+            subject=subject, sender=user_email,
+            recipients=['jack.m.gman@gmail.com'],
+            body=message,
+            reply_to=user_email)
         mail.send(email_msg)
     return app.send_static_file('support.html')
+
 
 @app.route('/cb')
 def default_home():
     """ Render the authorization code for user"""
     code = request.args.get('code')
-    return render_template('oauth.html', code = code)
-
-
-
-# if __name__ == '__main__':
-#     PORT = int(os.environ.get("PORT", 5000))
-#     app.run(host='0.0.0.0', port=PORT)
-
-
+    return render_template('oauth.html', code=code)
