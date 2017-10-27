bcacc0661670b625bf3a31ae90e41db8f80c4900
JohnGorm <jack.m.gman@gmail.com>
2017-10-27 13:58:43 -0500

    Hide abstracts till user clicks button

--- sciunit_webpage_oauth/static/js/app.js
+++ sciunit_webpage_oauth/static/js/app.js
@@ -1,9 +1,17 @@
-var $root = $('html, body');
-$('#stickyContents li').on('click','a', function(e){ 
-   e.preventDefault();
-   let href = $.attr(this, 'href');
-   $root.animate({
-       scrollTop: $(href).offset().top
-   }, 500)
-   return false;
-});
+$(document).ready(function(){
+    var $root = $('html, body');
+    $('#stickyContents li').on('click','a', function(e){ 
+       e.preventDefault();
+       let href = $.attr(this, 'href');
+       $root.animate({
+           scrollTop: $(href).offset().top
+       }, 500)
+       return false;
+    });
+    
+    
+    $('.abstract-toggle').click(function(){
+        console.log('toggle');
+        $(this).parent().children('.paper-abstract').toggle("display")
+    })
+})
